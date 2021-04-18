c9aa0571f1b8b0bb1f2edf9f8909d921ddc06253
David Frickert <david.frickert@protonmail.com>
2021-04-18 16:30:28 +0100

    benchmark with 'ab' tool

---
+++ docker-compose/benchmark/run.sh
@@ -0,0 +1,4 @@
+ab -n50 -c12 -p data -T application/json \
+ -H "Authorization: Basic MjNiYzQ2YjEtNzFmNi00ZWQ1LThjNTQtODE2YWE0ZjhjNTAyOjEyM3pPM3haQ0xyTU42djJCS0sxZFhZRnBYbFBrY2NPRnFtMTJDZEFzTWdSVTRWck5aOWx5R1ZDR3VNREdJd1A=" \
+ -H "Content-Type: application/json" -H "User-Agent: OpenWhisk-CLI/1.0 (2019-09-23T17:46:38.323+0000) linux 386"  \
+ https://192.168.1.8/api/v1/namespaces/_/actions/teste?blocking=false&result=true
