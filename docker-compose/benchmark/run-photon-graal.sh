bdbd49eeb84cc18badaacc7065a8052297582fba
dfrickert <david.frickert@premium-minds.com>
2021-04-25 12:53:31 +0100

    ow: add benchmark scripts for sleep function

--- docker-compose/benchmark/run-photon-graal.sh
+++ docker-compose/benchmark/run-photon-graal.sh
@@ -1,6 +1,4 @@
-ab -n500000 -c18 -p data -T application/json \
+ab -t600 -c18 -p data-sleep-graal -T application/json \
  -H "Authorization: Basic MjNiYzQ2YjEtNzFmNi00ZWQ1LThjNTQtODE2YWE0ZjhjNTAyOjEyM3pPM3haQ0xyTU42djJCS0sxZFhZRnBYbFBrY2NPRnFtMTJDZEFzTWdSVTRWck5aOWx5R1ZDR3VNREdJd1A=" \
  -H "Content-Type: application/json" -H "User-Agent: OpenWhisk-CLI/1.0 (2019-09-23T17:46:38.323+0000) linux 386"  \
- "https://localhost/api/v1/namespaces/_/actions/teste?blocking=true&result=true"
-
-# -v 3
+ "https://localhost/api/v1/namespaces/_/actions/sleep-photon-graal?blocking=true&result=true"
