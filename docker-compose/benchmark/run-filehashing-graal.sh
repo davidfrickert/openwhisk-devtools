45e6fa802a92bcadd1ce1daddd2153a298f82733
David Frickert <david.frickert@protonmail.com>
2021-06-13 17:01:02 +0100

    add simple benchmarks for filehashing

---
+++ docker-compose/benchmark/run-filehashing-graal.sh
@@ -0,0 +1,4 @@
+ab -n 5000 -c1 -p data-filehashing -T application/json \
+ -H "Authorization: Basic MjNiYzQ2YjEtNzFmNi00ZWQ1LThjNTQtODE2YWE0ZjhjNTAyOjEyM3pPM3haQ0xyTU42djJCS0sxZFhZRnBYbFBrY2NPRnFtMTJDZEFzTWdSVTRWck5aOWx5R1ZDR3VNREdJd1A=" \
+ -H "Content-Type: application/json" -H "User-Agent: OpenWhisk-CLI/1.0 (2019-09-23T17:46:38.323+0000) linux 386"  \
+ "https://localhost/api/v1/namespaces/_/actions/filehashing-graal?blocking=true&result=true"
