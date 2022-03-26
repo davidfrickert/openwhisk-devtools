0f3e45cb8ffd52671fc803b07c6e3657b4743501
David Frickert <david.frickert@protonmail.com>
2022-03-26 18:13:18 +0000

    remove result

--- docker-compose/benchmark/azure-dataset-benchmark-scripts/ow.py
+++ docker-compose/benchmark/azure-dataset-benchmark-scripts/ow.py
@@ -3,7 +3,7 @@ from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
 import requests as requests
 
 ip_address = '146.193.41.200'
-url = f'https://{ip_address}/api/v1/namespaces/_/actions/%s?blocking=true&result=true'
+url = f'https://{ip_address}/api/v1/namespaces/_/actions/%s?blocking=true&result=false'
 headers = {
     "Authorization": "Basic MjNiYzQ2YjEtNzFmNi00ZWQ1LThjNTQtODE2YWE0ZjhjNTAyOjEyM3pPM3haQ0xyTU42djJCS0sxZFhZRnBYbFBrY2NPRnFtMTJDZEFzTWdSVTRWck5aOWx5R1ZDR3VNREdJd1A=",
     "Content-Type": "application/json",
