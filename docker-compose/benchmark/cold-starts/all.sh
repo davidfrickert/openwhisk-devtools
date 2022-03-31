deaa2d1ba66a4fe6bc63fb206fc6d753016b8991
David Frickert <david.frickert@protonmail.com>
2022-03-31 13:29:20 +0100

    all coldstart run options

---
+++ docker-compose/benchmark/cold-starts/all.sh
@@ -0,0 +1,4 @@
+./run.sh sleep ch.ethz.systems.Sleep ../data-sleep
+./run.sh filehashing ch.ethz.systems.FileHashing ../data-filehashing
+./run.sh rest-api ch.ethz.systems.Login ../data-rest
+./run.sh video ch.ethz.systems.FFMPEG ../data-video
